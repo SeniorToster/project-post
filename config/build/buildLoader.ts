a09e136cfd7241f3a2096bea623c80da197fbbac
SeniorToster <seraleks04@gmail.com>
2023-06-20 17:40:55 +0300

    установка Babel

--- config/build/buildLoader.ts
+++ config/build/buildLoader.ts
@@ -1,8 +1,26 @@
 import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
-
 export function buildLoader({isDev}: BuildOptions) : webpack.RuleSetRule[] {
+
+    const babelLoader = {
+        test: /\.([jt]sx?$)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [isDev && require.resolve('react-refresh/babel')].filter(Boolean),
+            }
+        }
+    }
+
+
+    const typeScriptLoader = {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+    }
 
     const svgLoader ={
                 test: /\.svg$/i,
@@ -19,33 +37,30 @@ export function buildLoader({isDev}: BuildOptions) : webpack.RuleSetRule[] {
             ],
         }
 
-    const typeScriptLoader = {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }
-
-    const scssLoader = {
-              test: /\.s[ac]ss$/i,
-              use: [
-                  MiniCssExtractPlugin.loader,
-                  {
-                      loader: "css-loader",
-                      options: {
-                          modules: {
-                              auto: ((resourcePath: string) => Boolean(resourcePath.includes('.module.'))),
-                              localIdentName: isDev ? '[local]__[name]_[hash:base64:5]' : '[hash:base64:8]'
-                          }
-                      },
-                  },
-                  "sass-loader",
-              ],
-        }
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
+                    },
+                }
+            },
+            "sass-loader",
+        ],
+    }
 
     return [
+        babelLoader,
+        typeScriptLoader,
         fileLoader,
         svgLoader,
-        typeScriptLoader,
-        scssLoader,
+        cssLoader,
     ]
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,15 +2,23 @@ import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import {BuildOptions, BuildPaths} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin  from "@pmmmwh/react-refresh-webpack-plugin";
 
-export function buildPlugins (paths : BuildPaths, options: BuildOptions ) : webpack.WebpackPluginInstance[] {
-   return [
-        new webpack.ProgressPlugin(),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(options.isDev)
-        }),
-        new HtmlWebpackPlugin({ template: paths.html }),
-        new MiniCssExtractPlugin({filename: 'css/[name].[contenthash:8].css',
+export function buildPlugins (paths : BuildPaths, {isDev}: BuildOptions ) : webpack.WebpackPluginInstance[] {
+   const plugins = [
+       new webpack.ProgressPlugin(),
+       new webpack.DefinePlugin({
+           __IS_DEV__: JSON.stringify(isDev)
+       }),
+       new HtmlWebpackPlugin({ template: paths.html }),
+       new MiniCssExtractPlugin({filename: 'css/[name].[contenthash:8].css',
            chunkFilename: 'css/[name].[contenthash:8].css'})
-    ]
+   ]
+
+    if (isDev) {
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(new ReactRefreshWebpackPlugin())
+    }
+
+    return plugins
 }
