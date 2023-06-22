55c556fd9bea9a05aecb5db8f07d68967b30433d
SeniorToster <seraleks04@gmail.com>
2023-06-22 18:43:34 +0300

    добавлен плагин Webpack Bundle Analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,13 +3,14 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import { type BuildOptions, type BuildPaths } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
-
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 export function buildPlugins (paths: BuildPaths, { isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
     const plugins = [
         new webpack.ProgressPlugin(),
         new webpack.DefinePlugin({
             _IS_DEV_: JSON.stringify(isDev)
         }),
+
         new HtmlWebpackPlugin({ template: paths.html }),
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
@@ -18,8 +19,11 @@ export function buildPlugins (paths: BuildPaths, { isDev }: BuildOptions): webpa
     ]
 
     if (isDev) {
-        plugins.push(new webpack.HotModuleReplacementPlugin())
-        plugins.push(new ReactRefreshWebpackPlugin())
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin(),
+            new ReactRefreshWebpackPlugin(),
+            new BundleAnalyzerPlugin({ openAnalyzer: false })
+        )
     }
 
     return plugins
