7efe5ea3756fca539aef25c1b4824db946fd84e5
SeniorToster <seraleks04@gmail.com>
2023-07-03 18:11:30 +0300

    исправлены баги

--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -3,17 +3,19 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { useState } from 'react'
 import { Button } from 'shared/ui/Button/Button'
 import { LangSwitcher } from 'features/LangSwitcher'
+import { useTranslation } from 'react-i18next'
 
 interface SidebarProps {
     className?: string
 }
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false)
+    const { t } = useTranslation()
     const onChangCollapsed = () => { setCollapsed((prev) => !prev) }
 
     return (
         <div data-testid='Sidebar' className={classNames({ cls: styles.Sidebar, mods: { [styles.collapsed]: collapsed }, additional: [className] })}>
-            <Button data-testid='Sidebar-toggle' onClick={onChangCollapsed} theme='invers'>кнопка1</Button>
+            <Button data-testid='Sidebar-toggle' onClick={onChangCollapsed} theme='invers'>{t('кнопка')}</Button>
             <LangSwitcher/>
         </div>
     )
